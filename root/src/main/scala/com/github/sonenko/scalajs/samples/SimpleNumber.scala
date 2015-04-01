5a921d7387dde85059f2998af8d11f6e02a1d821
sonenko <s.onenko@thenewmotion.com>
2015-04-01 19:39:52 +0300

    add Simple number function

--- root/src/main/scala/com/github/sonenko/scalajs/samples/SimpleNumber.scala
+++ root/src/main/scala/com/github/sonenko/scalajs/samples/SimpleNumber.scala
@@ -1,6 +1,8 @@
 package com.github.sonenko.scalajs.samples
 
-class SimpleNumber {
+import scala.annotation.tailrec
+
+object SimpleNumber {
 
   /**
    * pure JS realization example
@@ -26,5 +28,23 @@ class SimpleNumber {
   }
    */
 
+  def simpleNumbersStream(count: Int): Vector[Int] = {
+    require(count >=0, "can generate vector of negative length")
+    def stream(x: Int, acc: List[Int]): Stream[Int] =
+      if (acc.exists(y => x % y == 0 && y != 1)) stream(x + 1, acc)
+      else x #:: stream(x + 1, x :: acc)
+    stream(1, Nil).take(count).toVector
+  }
+
+
+  def simpleNumbersTailRec(count: Int): Vector[Int] = {
+    require(count >=0, "can generate vector of negative length")
+    @tailrec
+    def rec(x: Int, acc: Vector[Int]): Vector[Int] =
+      if (acc.length >= count) acc
+      else if (acc.exists(y => x % y == 0 && y != 1)) rec(x + 1, acc)
+      else rec(x + 1, acc :+ x)
+    rec(1, Vector[Int]())
+  }
 
 }
--- root/src/test/scala/com/github/sonenko/scalajs/samples/SimpleNumberTest.scala
+++ root/src/test/scala/com/github/sonenko/scalajs/samples/SimpleNumberTest.scala
@@ -1,6 +1,45 @@
 package com.github.sonenko.scalajs.samples
 
+import utest._
+import utest.framework.{TestSuite, Test}
+import utest.util.Tree
+import scala.util.Try
 
-class SimpleNumberTest {
 
+object SimpleNumberF {
+  def test(name: String, function: Int => Vector[Int]): Tree[Test] = {
+
+    TestSuite {
+      s"$name" - {
+        "should return empty vector if argument is 0" - {
+          assert(
+            function(0) == Vector[Int]()
+          )
+        }
+        "Throw exception if negative argument provided" - {
+          assert(
+            Try(function(-1)).isFailure
+          )
+        }
+        "return Vector(1) if argument is 1" - {
+          assert(
+            function(1) == Vector(1)
+          )
+        }
+        "return Vector(1, 2, 3, 5, 7, 11, 13, 17, 19, 23) if argument is 10" - {
+          assert(
+            function(10) == Vector(1, 2, 3, 5, 7, 11, 13, 17, 19, 23)
+          )
+        }
+      }
+    }
+  }
+}
+
+object SimpleNumbersStreamTest extends TestSuite {
+  val tests = SimpleNumberF.test("simpleNumbersStream", SimpleNumber.simpleNumbersStream)
+}
+
+object SimpleNumbersTailRecTest extends TestSuite {
+  val tests = SimpleNumberF.test("simpleNumbersTailRec", SimpleNumber.simpleNumbersTailRec)
 }
